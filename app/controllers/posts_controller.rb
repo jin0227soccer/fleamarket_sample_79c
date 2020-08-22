5dc8aab63e92f3a817f7d22916acd6a6ffa33fff
jin0227soccer <jinsoccer0520@gmail.com>
2020-08-22 19:05:20 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
     @comments = @post.comments.includes(:user)
     @category = @post.category
     @like = Like.new
-    if user_singed_in?
+    if user_signed_in?
       @current_like = @post.likes.where(user_id: current_user.id).first
     end
   end
